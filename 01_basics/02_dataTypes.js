820be2c1f81af209603d9010f8d1f6cd105a7712
Vikas Kashyap <159269579+princeessjay@users.noreply.github.com>
2024-07-04 08:44:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all  JS code as newer version
+
+// alert("3 + 3") // We are using nodejs not browser
+
+console.log(3
+    +
+          3) // code readbilty should be high
+
+console.log("Vikas")
+
+let name = "Viaks"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// biging 
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+//object 
+
+console.log(typeof null); // object
+console.log(undefined); // undefinded
