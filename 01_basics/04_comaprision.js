7270f28fb7634c7295ae34c0acc616b4c4f438a4
Vikas Kashyap <159269579+princeessjay@users.noreply.github.com>
2024-07-07 08:45:59 +0000

    main

---
+++ 01_basics/04_comaprision.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1 ); // true
+// console.log("02" > 1); // true
+
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true 
+
+// with undefined all values will come with - false
+
+
