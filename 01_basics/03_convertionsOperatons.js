2ac06a627d13b9731c95e1cebd47824d3eb2d53b
Vikas Kashyap <159269579+princeessjay@users.noreply.github.com>
2024-07-07 08:27:33 +0000

    conversions

--- 01_basics/03_convertionsOperatons.js
+++ 01_basics/03_convertionsOperatons.js
@@ -1,35 +1,35 @@
 let score = 33
-console.log(typeof score); // number
-console.log(typeof (score)); // number
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
  
 let rupees = "50"
-console.log(typeof rupees); // string
+// console.log(typeof rupees); // string
 let valueInNumber = Number(rupees)
-console.log(typeof valueInNumber); // number
+// console.log(typeof valueInNumber); // number
 
 let any = "50abc"
 let valueInNumber2 = Number(any)
-console.log(valueInNumber2); // NaN
+// console.log(valueInNumber2); // NaN
 
 let any2 = null
 let valueInNumber3 = Number(any2)
-console.log(valueInNumber3); // 0
+// console.log(valueInNumber3); // 0
 
 let any3 = undefined
 let valueInNumber4 = Number(any3)
-console.log(valueInNumber4); // NaN
+// console.log(valueInNumber4); // NaN
 
 let boolean = true                         // if false
 let valueInNumber5 = Number(boolean)
-console.log(valueInNumber5); // 1          // 0 (if false)
+// console.log(valueInNumber5); // 1          // 0 (if false)
 
 let naam = "Vikas"
 let valueInNumber6 = Number(naam)
-console.log(valueInNumber6); // NaN
+// console.log(valueInNumber6); // NaN
 
 let isLooggedIn = ""
 let booIsLoggedIn = Boolean(isLooggedIn)
-console.log(booIsLoggedIn); // false
+// console.log(booIsLoggedIn); // false
 
 //1 => true ; 0 => false
 // "" => false
@@ -37,5 +37,29 @@ console.log(booIsLoggedIn); // false
 
 let someNumber = 33
 let stringsomeNumber = String(someNumber)
-console.log(stringsomeNumber); // 33
-console.log(typeof stringsomeNumber); // strings 
+// console.log(stringsomeNumber); // 33
+// console.log(typeof stringsomeNumber); // strings 
+
+
+// *************************** Operations*********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);    // -3 
+
+let str1 = 3
+let str2 = 2
+let str3 = str1+str2
+// console.log(str3);      // 5
+
+// console.log("1" + 2);         //12
+// console.log("1" + 2 + 2);     // 122
+// console.log(1 + 2 + "2");     //32
+
+// console.log(+true)  // 1
+// console.log(+"");   // 0
+
+let gameCounter = 100
+gameCounter++ // 101 
+++gameCounter //101
+// console.log(gameCounter); // 101
