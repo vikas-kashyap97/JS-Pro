63c4f96c9430adbd850ed36c444633d053406fc7
Vikas Kashyap <159269579+princeessjay@users.noreply.github.com>
2024-07-12 07:24:03 +0000

    while and do while

---
+++ 05_Iterations(loops)/while_and_do_while_loops.js
@@ -0,0 +1,28 @@
+// while 
+
+
+let index = 0
+while (index<= 10) {
+    // console.log(`Vlaue of index is ${index}`);             // Vlaue of index is 0 to 10      
+    index = index + 1
+}
+
+
+let myArray = ["flash", "hulk", "ironman", "superman"]
+
+
+let myArr = 0
+while (myArr < myArray.length) {
+    console.log(`My superheroes is ${myArray[myArr]}`);     // My superheroes is flash hulk ironman superman
+    myArr = myArr + 1
+}
+
+
+// do while
+
+let score = 0
+
+do {
+    console.log(`Score is ${score}`);                      // score is 1 to 10
+    score ++
+} while (score <= 10);
