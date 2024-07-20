12f95479a55831ecbfd1b73e63c4d418262f124a
Vikas Kashyap <159269579+princeessjay@users.noreply.github.com>
2024-07-20 08:54:29 +0000

    fn.call(this,param)

---
+++ 10_Object_oriented_programming/call.js
@@ -0,0 +1,14 @@
+function SetUsername(username){
+    // complex DB calls
+    this.username = username
+    console.log("called");
+}
+function createUsername(username,email,password){
+    // SetUsername(username)                            // if you give only this for calling the outside object into this , it will not work
+    SetUsername.call(this,username)                     // for calling this you have to wirte syntax like this- "fn.call(this,param)"
+
+    this.email = email
+    this.password = password
+}
+const my = new createUsername("Vikas", "Vikas@google.com", "123")
+console.log(my);
